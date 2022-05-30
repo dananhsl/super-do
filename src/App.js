9f0e65e65d90edb43a318896ded7e9df7a2c7df7
Dana Neuhäusel <skadi@Danas-MacBook-Air.local>
2022-05-30 18:03:08 +0200

    add form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Form from "./components/Form";
+import { nanoid } from "react";
+import Display from "./components/Display";
+//import  Notes  from "./components/ToDo";
 
 function App() {
+  const [options, setOptions] = useState("");
+
+  function addOption(optionString) {
+    console.log(optionString);
+    const newOptionObject = {
+      id: nanoid(),
+      text: optionString,
+      isOk: false,
+    };
+    setOptions([...options, newOptionObject]);
+  }
+  function deleteOption(id) {
+    console.log(id);
+    const updatedOptions = options.filter((option) => !(option.id === id));
+    setOptions(updatedOptions);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <Form onSubmit={addOption} />
     </div>
   );
 }
